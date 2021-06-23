51cedffc46702a3317c2477dec4abbff800ddaf7
Brody Beebe <bbeebe13@gmail.com>
2021-06-22 22:00:58 -0600

    added answer buttons using DOM

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,35 @@ var quizQuestionsObj =
         question5: "A very useful tool used during development and debugging for printing content to the debugger is?"
     }
 
+var answersObj = [
+    {
+        answer1: "L",
+        answer2: "B",
+        answer3: "C",
+        answer4: "D",
+    }, 
+    {
+        answer1: "A",
+        answer2: "B",
+        answer3: "C",
+        answer4: "D",
+    },
+    {
+        answer1: "A",
+        answer2: "B",
+        answer3: "C",
+        answer4: "D",
+    },
+    {
+        answer1: "A",
+        answer2: "B",
+        answer3: "C",
+        answer4: "D",
+    }
+]
+
+console.log(answersObj);
+
 // Timer function starts at 75 and counts down to 0
 var timerCounter = function() {
     var remainingTime = 75;
@@ -24,19 +53,49 @@ var timerCounter = function() {
 }
 
 //Displays quiz questions box
-var createQuizQuestions = function() {
+var createQuizQuestions = function(question, answers) {
     //Creates quiz container element
     var quizContainerEl = document.createElement("section");
     quizContainerEl.classList.add("quiz-container");
     body.appendChild(quizContainerEl);
 
     var quizQuestionEl = document.createElement("h2");
-    quizQuestionEl.textContent = "Question 1";
+    quizQuestionEl.textContent = question;
 
     quizContainerEl.appendChild(quizQuestionEl);
+
+    var answerButtonEl = document.createElement("div");
+    answerButtonEl.classList.add("answer-section");
+
+    quizContainerEl.appendChild(answerButtonEl);
+
+    for (var i = 0; i < 4; i++) {
+        if (i===0) {
+            var answerButtons = document.createElement("button");
+            answerButtons.classList.add("answer-button");
+            answerButtons.textContent = answers.answer1;
+            answerButtonEl.appendChild(answerButtons);
+        } else if (i===1) {
+            var answerButtons = document.createElement("button");
+            answerButtons.classList.add("answer-button");
+            answerButtons.textContent = answers.answer2;
+            answerButtonEl.appendChild(answerButtons);
+        } else if (i===2) {
+            var answerButtons = document.createElement("button");
+            answerButtons.classList.add("answer-button");
+            answerButtons.textContent = answers.answer3;
+            answerButtonEl.appendChild(answerButtons);
+        } else {
+            var answerButtons = document.createElement("button");
+            answerButtons.classList.add("answer-button");
+            answerButtons.textContent = answers.answer4;
+            answerButtonEl.appendChild(answerButtons);
+        }
+
+    }
 }
 
 $("#startButton").on("click", function() {
     timerCounter();
-    createQuizQuestions();
+    createQuizQuestions(quizQuestionsObj.question1, answersObj[0]);
 })
