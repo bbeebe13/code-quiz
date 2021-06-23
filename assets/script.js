96efaf7f3ab6e3adb7e2c2f7de62782b5178d733
Brody Beebe <bbeebe13@gmail.com>
2021-06-22 20:54:16 -0600

    added questions object and function to display quiz questions

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,17 @@
 
-
+// queries the timer displey in HTML
 var timerDisplay = document.querySelector(".timer-text")
+var body = document.body
+var quizQuestionsObj =
+    {
+        question1: "Commonly used data types DO NOT include?",
+        question2: "The condition in an if/else statement is enclosed with?",
+        question3: "Arrays in Javascript can be used to store?",
+        question4: "String values must be enclosed within _______ when being assigned to variables?",
+        question5: "A very useful tool used during development and debugging for printing content to the debugger is?"
+    }
 
-
+// Timer function starts at 75 and counts down to 0
 var timerCounter = function() {
     var remainingTime = 75;
     var timer = setInterval(function(){
@@ -14,10 +23,20 @@ var timerCounter = function() {
     }, 1000)
 }
 
-var quizQuestions = function() {
-    
+//Displays quiz questions box
+var createQuizQuestions = function() {
+    //Creates quiz container element
+    var quizContainerEl = document.createElement("section");
+    quizContainerEl.classList.add("quiz-container");
+    body.appendChild(quizContainerEl);
+
+    var quizQuestionEl = document.createElement("h2");
+    quizQuestionEl.textContent = "Question 1";
+
+    quizContainerEl.appendChild(quizQuestionEl);
 }
 
 $("#startButton").on("click", function() {
     timerCounter();
+    createQuizQuestions();
 })
